db2da0f026093587b8e36376064a0bf619cda66c
Pedro Lucas <pdrolucas.dev@gmail.com>
2023-04-26 13:47:12 -0300

    Adicionar arquivos de projeto.

---
+++ learn-csharp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Conditional Operator
+using System;
+using System.Runtime.InteropServices;
+
+int purchase = 1000;
+
+Console.WriteLine(purchase >= 1000 ? "You are elegible for U$100 of discount" : "You are elegible for U$50 of discount");
